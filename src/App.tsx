bc62fe4fb1b3097a81c05b50c5d925a581926b4d
chewmountain <mironov.develop@gmail.com>
2023-05-23 14:06:59 +0300

    2023-05-23

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Counter } from "./components/Counter";
-import "./index.scss";
+import { Suspense, useContext, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./styles/index.scss";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
-      Hello world!
-      <Counter />
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Switch theme</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О нас</Link>
+      <Suspense fallback={"Loading..."}>
+        <Routes>
+          <Route path="/about" element={<AboutPageLazy />} />
+          <Route path="/" element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
